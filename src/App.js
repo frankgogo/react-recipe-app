e859dea07febe6c4160a27f661d86c9255e8ddab
frankgogo <iplay098310@gmail.com>
2019-09-13 09:23:47 +0800

    env-variable-update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home'
+import Recipes from './pages/Recipes'
+import SingleRecipe from './pages/SingleRecipe'
+import Default from './pages/Default'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-      <div>hello recipes app!</div>
+      <Router>
+          <main>
+              {/* navbar */}
+              <Navbar/>
+              <Switch>
+                  <Route path="/" exact component={Home} />
+                  <Route path="/recipes" exact component={Recipes} />
+                  <Route path="/recipes/:id" component={SingleRecipe} />
+                  <Route component={Default} />
+              </Switch>
+          </main>
+      </Router>
   );
 }
 
---
+++ src/pages/Recipes.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import RecipeList from "../components/RecipeList";
+import Search from "../components/Search"
+import { recipeData } from "../data/tempList";
+
+export default class Recipes extends Component {
+    state = {
+        recipes: recipeData,
+        search: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            search: e.target.value
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <>
+                <Search 
+                    search={this.state.search} 
+                    handleChange={this.handleChange} 
+                    handleSubmit={this.handleSubmit}
+                />
+                <RecipeList recipes={this.state.recipes} />
+            </>
+        );
+    }
+}
