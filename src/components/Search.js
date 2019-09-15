bdbe1e6cf662a023da3cbbbc6b320b671d21f5b6
frankgogo <iplay098310@gmail.com>
2019-09-15 08:02:25 +0800

    search-bar-adjust

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,7 @@ export default class Search extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-10 mx-auto col-md-8 mt-5 text-center">
+                    <div className="col-10 mx-auto col-md-12 mt-5 text-center">
                         <h1 className="text-slanted text-capitalize">
                             search recipes with {" "}
                             <strong className="text-orange">Food2Fork</strong>
@@ -15,23 +15,23 @@ export default class Search extends Component {
                             <label htmlFor="search" className="text-capitalize">
                                 types recipes separated by comma
                             </label>
-                            <div className="input-group">
-                                <input 
-                                    type="text" 
-                                    name="search" 
-                                    className="form-control-lg" 
-                                    placeholder="chicken,onion,carrots"
-                                    value={search}
-                                    onChange={handleChange}
+                            <div className="input-group mt-3">
+                                <input
+                                type="text"
+                                name="search"
+                                className="form-control"
+                                placeholder="chicken,onion,carrots"
+                                value={search}
+                                onChange={handleChange}
                                 />
                                 <div className="input-group-append">
-                                    <button 
-                                        type="submit" 
-                                        className="input-group-text bg-primary text-white"
-                                        onClick={handleSubmit}
-                                    >
-                                        <i className="fas fa-search"></i>
-                                    </button>
+                                <button
+                                    type="submit"
+                                    className="input-group-text bg-primary text-white"
+                                    onClick={handleSubmit}
+                                >
+                                    <i className="fas fa-search" />
+                                </button>
                                 </div>
                             </div>
                         </form>
